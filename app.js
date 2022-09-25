cfe7fa41c20b3a0b4c70b49ca6a2de588b006a6f
Eva S <evamarie.songco@gmail.com>
2022-09-25 00:16:43 -0700

    Added number, operator checks as input to calculate function

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ console.log ("Initialize...");
 let num1;
 let num2;
 let operator;
-let equation = `Perform a basic calculation between 2 numbers, num1 ${num1} and num2 ${num2} using ${operator} (+, -, x, /)`;
-
+let num1Str = "";
+let num2Str = "";
 let result;
 let numEntry = "";
 let outputText;
-
-// let dotPos;
-// let position;
-// let dotCt = 0;
+let dotCt = 0;
 
 // Grabbed elements from HTML
 
@@ -26,10 +23,8 @@ const buttonItems = document.querySelectorAll(".input");
 // const AC = document.getElementById("clear");
 // const calculate = document.getElementById("calc");
 
-console.log(inputText);
-console.log (resultText);
-console.log (inputButtons);
-console.log (buttonItems);
+// console.log(inputText);
+// console.log (resultText);
 
 // Functions
 // BASIC CALCULATOR FUNCTIONS: ADD, SUBTRACT, MULTIPLY, DIVIDE
@@ -39,8 +34,18 @@ const addTwoNumbers = (num1,num2) => num1 + num2;
 const subtractTwoNumbers = (num1,num2) => num1 - num2;
 const multiplyTwoNumbers = (num1,num2) => num1 * num2;
 const divideTwoNumbers  = (num1,num2) => num1 / num2;
+const percentTwoNumbers = (num1,num2) => num1 * (num2/100);
 
 const calculateTwoNumbers = (num1, operator, num2) => {
+    console.log (`Perform a basic calculation between 2 numbers, num1 ${num1} and num2 ${num2} using ${operator} (+, -, x, /)`);
+    num1 = Number(num1Str);
+    num2 = Number(num2Str);
+    operator;
+
+    console.log(`Input 1 is ${num1}`);
+    console.log(`Input 2 is ${num2}`);
+    console.log(`Operator is ${operator}`);
+
     switch (operator) {
         case "+":
             result = addTwoNumbers (num1, num2);
@@ -54,11 +59,14 @@ const calculateTwoNumbers = (num1, operator, num2) => {
         case "/":
             result = divideTwoNumbers (num1, num2);
             break;
+        case "%":
+            result = percentTwoNumbers (num1, num2);
+            break;
         default:
             break;
     }
     return (result);
-    resultText.innerHTML=result;
+    console.log (result);
 };
 
 // PERFORM BASIC CALCULATOR MOUSE CLICKS 
@@ -68,7 +76,7 @@ buttonItems.forEach(buttonItem => {
 
 // TAKE VALUE OF MOUSE CLICKS, SHOW APPENDED NUMBERS ON TOP DISPLAY AREA
     const outputText = buttonItem.textContent;
-    console.log(outputText);
+    // console.log(outputText);
     switch(outputText) {
         case "0":
         case "1":
@@ -80,46 +88,51 @@ buttonItems.forEach(buttonItem => {
         case "7":
         case "8":
         case "9":
-        case ".":
             numEntry = numEntry + outputText;
             resultText.innerHTML=numEntry;
+            console.log(resultText.innerHTML);
             break;
         case ".":
-            // if (dotCt === 0) {
-            //     numEntry = numEntry + outputText;
-            //     resultText.innerHTML = numberStr;
-            //     dotCt ++;
-            // }
+            if (dotCt === 0) {
+                numEntry = numEntry + outputText;
+                resultText.innerHTML = numEntry;
+                dotCt ++;
+            } 
             break;
 
         case "AC": // CLEAR DISPLAY AREA
             numEntry = "";
-            resultText.innerHTML=numEntry;
+            resultText.innerHTML = numEntry;
+            inputText.innerHTML = numEntry;
         case "+":
         case "-": 
         case "*": 
-        case "/":     
-            num1 = Number(numEntry);
-            num2 = Number(numEntry);
+        case "/":
             operator = outputText;
+            if (num1Str === "") {
+                num1Str = operator + numEntry;
+                num1 = Number(num1Str);
+            } else {
+                num2Str = operator + numEntry;
+                num2 = Number(num2Str);
+                outputText.innerHTML = num2Str;
+            }
             numEntry = numEntry + outputText;
             inputText.innerHTML = numEntry;
-            resultText.innerHTML = calculateTwoNumbers (num1, operator, num2);
-            console.log(num1);
-            console.log(operator);
-            console.log(numEntry);
+            numEntry = "";
+            operator = outputText;
             break;
         case "=":
             num2 = Number(numEntry);
             numEntry = numEntry + outputText;
-            resultText.innerHTML = calculateTwoNumbers(num1, operator, num2); 
-            equation = `${num1} ${operator} ${num2} =`
-            console.log (num2);
-            console.log (operator);
-            console.log (outputText);
-            console.log (equation);
-            console.log (numEntry);
-            console.log (resultText);
+            equation = `${num1} ${operator} ${num2}`;
+            console.log(`num1= ${num1}`);
+            console.log(`operator = ${operator}`);
+            console.log(`num2 = ${num2}`);
+            console.log(`num2Str = ${num2Str}`);
+            console.log (`equation = ${equation}`);
+            resultText.innerHTML = calculateTwoNumbers(`${num1}, ${operator}, ${num2}`); 
+            console.log(resultText.innerHTML);
             break;
         case "+/-":
         case "%":
@@ -127,18 +140,9 @@ buttonItems.forEach(buttonItem => {
         default: 
             break;
     }
-
-// My checks
-console.log (resultText);
-// console.log (AC);
-// console.log(calculate);
-console.log(inputButtons);
-console.log(buttonItems);
-// console.log (`${equation} =`)
-// console.log (calculateTwoNumbers(num1,operator,num2));
+    
 
 });
-
 
 // -----------------------------------------------------------
 // const num1 = Number(prompt("First number: "));
