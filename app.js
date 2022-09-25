e89b7b1845f2ba92e46d1cc7d0263b4b0e030b3b
Eva S <evamarie.songco@gmail.com>
2022-09-24 21:01:11 -0700

    Added input display in html, defined variables for calculation in js

--- app.js
+++ app.js
@@ -1,44 +1,40 @@
-// THESE ARE THE HTML ELEMENTS I WANT / NEED
-// GOOD - RESULT LINE
-const showResult = document.getElementById("output-display");
-console.log (showResult);
-// GOOD - A/C BUTTON
-const clearButton = document.getElementById("clear");
-console.log (clearButton);
-// GOOD = EQUAL BUTTON
-const calcButton = document.getElementById("calc");
-console.log(calcButton);
-// GOOD ALL BUTTONS - ELEMENTS
-const inputButtons = document.getElementsByClassName("input");
-console.log(inputButtons);
-// GOOD ALL BUTTONS - NODE LIST
-const buttonItems = document.querySelectorAll(".input");
-console.log(buttonItems);
+// My Declared Variables
 
-// -------------------------------------------------------------------
-// 
-// GOOD - BYPASS PAGE REFRESH ON CLICK / ENTER
-buttonItems.forEach(buttonItem => {
-    buttonItem.addEventListener("click", (event) => {
-    event.preventDefault();
+console.log ("Initialize...");
 
-    outputText = buttonItem.textContent;
+let num1;
+let num2;
+let operator;
+let equation = `Perform a basic calculation between 2 numbers, num1 ${num1} and num2 ${num2} using ${operator} (+, -, x, /)`;
 
-    const entryItem = document.getElementsByClassName("input");
-    entryItem[0].innerHTML = outputText;
-     })
-});
-
-// const entryItem = document.getElementsByClassName("input");
-// entryItem[0].innerHTML = outputText;
-
-const num1 = Number(prompt("First number: "));
-const operator = prompt("Operator: ");
-const num2 = Number(prompt("Second number: "));
-
-let equation = `${num1} ${operator} ${num2}`;
 let result;
+let numEntry = "";
+let outputText;
 
+// let dotPos;
+// let position;
+// let dotCt = 0;
+
+// Grabbed elements from HTML
+
+console.log ("Grab elements...");
+
+const inputText = document.getElementById("input-display");
+const resultText = document.getElementById("output-display");
+const inputButtons = document.getElementsByClassName("input")[0];
+const buttonItems = document.querySelectorAll(".input");
+// const AC = document.getElementById("clear");
+// const calculate = document.getElementById("calc");
+
+console.log(inputText);
+console.log (resultText);
+console.log (inputButtons);
+console.log (buttonItems);
+
+// Functions
+// BASIC CALCULATOR FUNCTIONS: ADD, SUBTRACT, MULTIPLY, DIVIDE
+
+// SAVE RESULT OF CALCULATING TWO NUMBERS
 const addTwoNumbers = (num1,num2) => num1 + num2;
 const subtractTwoNumbers = (num1,num2) => num1 - num2;
 const multiplyTwoNumbers = (num1,num2) => num1 * num2;
@@ -46,7 +42,7 @@ const divideTwoNumbers  = (num1,num2) => num1 / num2;
 
 const calculateTwoNumbers = (num1, operator, num2) => {
     switch (operator) {
-        case "+": 
+        case "+":
             result = addTwoNumbers (num1, num2);
             break;
         case "-":
@@ -62,10 +58,93 @@ const calculateTwoNumbers = (num1, operator, num2) => {
             break;
     }
     return (result);
-}
+    resultText.innerHTML=result;
+};
 
-// alert (`${equation} = `);
-// alert (calculateTwoNumbers(num1,operator,num2));
+// PERFORM BASIC CALCULATOR MOUSE CLICKS 
+buttonItems.forEach(buttonItem => {
+    buttonItem.addEventListener("click", (event) => {
+    event.preventDefault();
 
-console.log (`${equation} =`)
-console.log (calculateTwoNumbers(num1,operator,num2));
+// TAKE VALUE OF MOUSE CLICKS, SHOW APPENDED NUMBERS ON TOP DISPLAY AREA
+    const outputText = buttonItem.textContent;
+    console.log(outputText);
+    switch(outputText) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+        case ".":
+            numEntry = numEntry + outputText;
+            resultText.innerHTML=numEntry;
+            break;
+        case ".":
+            // if (dotCt === 0) {
+            //     numEntry = numEntry + outputText;
+            //     resultText.innerHTML = numberStr;
+            //     dotCt ++;
+            // }
+            break;
+
+        case "AC": // CLEAR DISPLAY AREA
+            numEntry = "";
+            resultText.innerHTML=numEntry;
+        case "+":
+        case "-": 
+        case "*": 
+        case "/":     
+            num1 = Number(numEntry);
+            num2 = Number(numEntry);
+            operator = outputText;
+            numEntry = numEntry + outputText;
+            inputText.innerHTML = numEntry;
+            resultText.innerHTML = calculateTwoNumbers (num1, operator, num2);
+            console.log(num1);
+            console.log(operator);
+            console.log(numEntry);
+            break;
+        case "=":
+            num2 = Number(numEntry);
+            numEntry = numEntry + outputText;
+            resultText.innerHTML = calculateTwoNumbers(num1, operator, num2); 
+            equation = `${num1} ${operator} ${num2} =`
+            console.log (num2);
+            console.log (operator);
+            console.log (outputText);
+            console.log (equation);
+            console.log (numEntry);
+            console.log (resultText);
+            break;
+        case "+/-":
+        case "%":
+            break;
+        default: 
+            break;
+    }
+
+// My checks
+console.log (resultText);
+// console.log (AC);
+// console.log(calculate);
+console.log(inputButtons);
+console.log(buttonItems);
+// console.log (`${equation} =`)
+// console.log (calculateTwoNumbers(num1,operator,num2));
+
+});
+
+
+// -----------------------------------------------------------
+// const num1 = Number(prompt("First number: "));
+// const operator = prompt("Operator: ");
+// const num2 = Number(prompt("Second number: "));
+// const num1 = 100;
+// const operator = "/";
+// const num2 = 5;
+});
