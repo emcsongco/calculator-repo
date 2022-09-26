216ae9e66efd8dab0a745cf69db4ed84eaff264f
Eva S <evamarie.songco@gmail.com>
2022-09-26 08:48:59 -0700

    Moved AC action outside of switch case loop, calculate function takes the 2 parameters with the operator, and add function works

--- app.js
+++ app.js
@@ -18,13 +18,9 @@ console.log ("Grab elements...");
 
 const inputText = document.getElementById("input-display");
 const resultText = document.getElementById("output-display");
-const inputButtons = document.getElementsByClassName("input")[0];
 const buttonItems = document.querySelectorAll(".input");
-// const AC = document.getElementById("clear");
+const AC = document.getElementById("clear");
 // const calculate = document.getElementById("calc");
-
-// console.log(inputText);
-// console.log (resultText);
 
 // Functions
 // BASIC CALCULATOR FUNCTIONS: ADD, SUBTRACT, MULTIPLY, DIVIDE
@@ -37,13 +33,11 @@ const divideTwoNumbers  = (num1,num2) => num1 / num2;
 const percentTwoNumbers = (num1,num2) => num1 * (num2/100);
 
 const calculateTwoNumbers = (num1, operator, num2) => {
-    console.log (`Perform a basic calculation between 2 numbers, num1 ${num1} and num2 ${num2} using ${operator} (+, -, x, /)`);
-    num1 = Number(num1Str);
-    num2 = Number(num2Str);
-    operator;
-
-    console.log(`Input 1 is ${num1}`);
-    console.log(`Input 2 is ${num2}`);
+    console.log (`Calculate... ${num1} ${operator} ${num2}`);
+    
+    // operator;
+    console.log(`Input 1 ${num1} is a ${typeof num1}`)
+    console.log(`Input 2 ${num2} is a ${typeof num2}`)
     console.log(`Operator is ${operator}`);
 
     switch (operator) {
@@ -69,6 +63,7 @@ const calculateTwoNumbers = (num1, operator, num2) => {
     console.log (result);
 };
 
+
 // PERFORM BASIC CALCULATOR MOUSE CLICKS 
 buttonItems.forEach(buttonItem => {
     buttonItem.addEventListener("click", (event) => {
@@ -77,6 +72,20 @@ buttonItems.forEach(buttonItem => {
 // TAKE VALUE OF MOUSE CLICKS, SHOW APPENDED NUMBERS ON TOP DISPLAY AREA
     const outputText = buttonItem.textContent;
     // console.log(outputText);
+
+    // if outputText [0-9],[.] 
+    
+    //     if operator is null/blank:
+    //        append to string Array number1
+    //     else 
+    //         assign my string array to number2
+    
+    // else if outputText != "="
+    //     assign operator;
+
+    //     else
+    //         pass num1, num2, operator if equal
+        
     switch(outputText) {
         case "0":
         case "1":
@@ -97,42 +106,46 @@ buttonItems.forEach(buttonItem => {
                 numEntry = numEntry + outputText;
                 resultText.innerHTML = numEntry;
                 dotCt ++;
+                console.log(resultText.innerHTML);
             } 
             break;
-
-        case "AC": // CLEAR DISPLAY AREA
-            numEntry = "";
-            resultText.innerHTML = numEntry;
-            inputText.innerHTML = numEntry;
+        // case "AC": // CLEAR DISPLAY AREA
+        //     numEntry = "";
+        //     inputText.innerHTML = "";
+        //     resultText.innerHTML = numEntry;
+            
         case "+":
         case "-": 
         case "*": 
         case "/":
             operator = outputText;
+            console.log(operator);
             if (num1Str === "") {
                 num1Str = operator + numEntry;
                 num1 = Number(num1Str);
+                console.log(num1);
+                console.log(num1Str);    
             } else {
                 num2Str = operator + numEntry;
                 num2 = Number(num2Str);
                 outputText.innerHTML = num2Str;
+                console.log(num2);
+                console.log(outputText.innerHTML);    
             }
             numEntry = numEntry + outputText;
             inputText.innerHTML = numEntry;
             numEntry = "";
-            operator = outputText;
             break;
         case "=":
-            num2 = Number(numEntry);
-            numEntry = numEntry + outputText;
-            equation = `${num1} ${operator} ${num2}`;
-            console.log(`num1= ${num1}`);
-            console.log(`operator = ${operator}`);
-            console.log(`num2 = ${num2}`);
-            console.log(`num2Str = ${num2Str}`);
-            console.log (`equation = ${equation}`);
-            resultText.innerHTML = calculateTwoNumbers(`${num1}, ${operator}, ${num2}`); 
-            console.log(resultText.innerHTML);
+            if (operator != "=") {
+                num2Str = operator + numEntry;
+                num2 = Number(num2Str);
+                numEntry = numEntry + outputText;
+                equation = `${num1} ${operator} ${num2}`;
+                inputText.innerHTML = equation;
+                resultText.innerHTML = calculateTwoNumbers(num1, operator, num2); 
+                console.log(resultText.innerHTML);
+            }
             break;
         case "+/-":
         case "%":
@@ -140,10 +153,15 @@ buttonItems.forEach(buttonItem => {
         default: 
             break;
     }
-    
 
 });
+    AC.addEventListener("click",(event) => {
+    inputText.innerHTML = "";
+    resultText.innerHTML = "";
+    numEntry="";
+    return;
 
+})
 // -----------------------------------------------------------
 // const num1 = Number(prompt("First number: "));
 // const operator = prompt("Operator: ");
