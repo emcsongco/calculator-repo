72b5814484886297c77298d33b5887f75a04abf2
Eva S <evamarie.songco@gmail.com>
2022-09-27 12:37:51 -0700

    Added invalid checks around = and processing of signed numbers

--- app.js
+++ app.js
@@ -2,17 +2,20 @@
 
 console.log ("Initialize parameters...");
 
-let num;
-let num1;
-let num2;
+let num=0;
+let num1=0;
+let num2=0;
 let num1Str = "";
 let num2Str = "";
-let operator;
-let result;
+let operator="";
+let result=0;
 let dotClicked = false;
 let numberSign;
+let numberString="";
 let numEntry = "";
-let outputText;
+let outputText="";
+let equalClicked = false;
+opCtr = 0;
 
 // Grabbed elements from HTML
 
@@ -111,13 +114,20 @@ buttonItems.forEach(buttonItem => {
             console.log ("Operator processing...")
             operator = outputText;
             dotClicked = false;
+            opCtr ++;
             console.log(`   operator = ${operator}`);
+            console.log(`   operator Counter = ${opCtr}`);
+
             if (num1Str === "") {
                 if (operator = "+") {
                     num1Str = operator + numEntry;
+                    
+                    if (num1Str[1]=="-") {num1Str = numEntry;}
+
                 } else {
                     num1Str = numEntry + operator;
                 }    
+                console.log(`   num1Str = ${num1Str} (${typeof num1Str})`);
                 num1 = Number(num1Str);
                 console.log(`   num1 = ${num1} (${typeof num1})`);
                 console.log(`   num1Str = ${num1Str} (${typeof num1Str})`);
@@ -128,6 +138,7 @@ buttonItems.forEach(buttonItem => {
                 console.log(`   num2 = ${num2} (${typeof num2})`);
                 console.log(outputText.innerHTML);    
             }
+
             operator = outputText;
             numEntry = numEntry + outputText;
             inputText.innerHTML = numEntry;
@@ -140,8 +151,9 @@ buttonItems.forEach(buttonItem => {
                 console.log(`num = ${num} (${typeof num}`) ;
                 num = Number(num);
                 console.log(num);
+               
                 if (num >=0) {
-                    numEntry = posToNeg(num);;
+                    numEntry = posToNeg(num);
                 } else numEntry = NegToPos(num); 
                 resultText.innerHTML=numEntry;
 
@@ -153,19 +165,26 @@ buttonItems.forEach(buttonItem => {
 
         //CALCULATE RESULTS BASED ON GIVEN 2 NUMBER INPUTS AND THE OPERATOR    
         case "=":
+            if (opCtr !== 0) {
                 num2Str = numEntry;
                 num2 = Number(num2Str);
                 equation = `${num1} ${operator} ${num2}`;
                 numEntry = equation;
                 inputText.innerHTML = equation;
-                resultText.innerHTML = calculateTwoNumbers(num1, operator, num2); 
+                console.log(`   operator Counter = ${opCtr}`);
                 console.log(`Displayed equation for inputText.innerHTML = ${inputText.innerHTML}`);
                 console.log(`Displayed total for resultText.innerHTML = ${resultText.innerHTML}`);
+                resultText.innerHTML = calculateTwoNumbers(num1, operator, num2); 
+                opCtr = 0;
+                console.log(`   operator Counter = ${opCtr}`);
+                console.log(`Displayed equation for inputText.innerHTML = ${inputText.innerHTML}`);
+                console.log(`Displayed total for resultText.innerHTML = ${resultText.innerHTML}`);
+            }    
             break;
         default: 
             break;
     }
-
+    
 });
 
 AC.addEventListener("click",(event) => {
@@ -176,7 +195,7 @@ console.log(`Clear = inputText.innerHTML = ${inputText.innerHTML}`)
 console.log(`Clear = resultText.innerHTML = ${resultText.innerHTML}`)
 num1=0;
 num1Str = "";
-
+opCtr = 0;
 
 })
 
